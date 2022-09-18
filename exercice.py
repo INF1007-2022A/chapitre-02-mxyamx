e6a1463ab1b40485ffe3d8792c3f6dd3e135fe62
maya <aitmayamanel@gmail.com>
2022-09-18 19:42:34 -0400

    exercice chapitre 2 resolus

--- exercice.py
+++ exercice.py
@@ -3,19 +3,14 @@
 def majuscule(mot):
     # TODO completer la fonction ici
     return mot
+#Acquisition des donnees
+ma_chaine =input('Veuillez entrez une chaine de caractere:')
+#2 boucle sur les caracteres
+ma_nouvelle_chaine = ''
+for caractere in ma_chaine:
+    ma_nouvelle_chaine += chr(ord(caractere)-32)
+#sortie des donnes
+print(ma_nouvelle_chaine)
 
 
-if __name__ == '__main__':
-    mots = [
-        'riz',
-        'cours',
-        'voiture',
-        'oiseau',
-        'bonjour',
-        'églantier',
-        'arbre'
-    ]
-    for i in range(len(mots)):
-        mots[i] = majuscule(mots[i])
 
-    print(mots)
