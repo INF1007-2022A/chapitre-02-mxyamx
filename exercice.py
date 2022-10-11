ada977f9e923011c4d00ff5aade521498210e081
maya <aitmayamanel@gmail.com>
2022-10-11 10:33:55 -0400

    exercice 2 revisé

--- exercice.py
+++ exercice.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 def majuscule(mot):
-    # TODO completer la fonction ici
     return mot
-#Acquisition des donnees
-ma_chaine =input('Veuillez entrez une chaine de caractere:')
-#2 boucle sur les caracteres
+ma_chaine = input(' Veuillez entrez un mot : ')
 ma_nouvelle_chaine = ''
 for caractere in ma_chaine:
     ma_nouvelle_chaine += chr(ord(caractere)-32)
-#sortie des donnes
 print(ma_nouvelle_chaine)
+
 
 
 
